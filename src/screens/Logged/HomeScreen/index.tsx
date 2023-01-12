dffbe76ee239922e24af45f92c9b341da7f5ab34
João Leão <joaofleao@gmail.com>
2023-01-12 04:23:39 -0300

    feat: basic home screen added

---
+++ src/screens/Logged/HomeScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+function HomeScreen() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-blue-900'>Teste</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
