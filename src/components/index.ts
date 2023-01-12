d4e22248ee75e82e20d95840c0934588601394e3
João Leão <joaofleao@gmail.com>
2023-01-12 04:20:26 -0300

    refactor: components index fixes to encapsulate all current components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 import TextInputComponent from './TextInputComponent'
 import ButtonComponent from './ButtonComponent'
+import SocialButtonComponent from './SocialButtonComponent'
+import LogoComponent from './LogoComponent'
+import ModelComponent from './ModelComponent'
+import LoadingComponent from './LoadingComponent'
 
-export { TextInputComponent, ButtonComponent }
+export { TextInputComponent, ButtonComponent, SocialButtonComponent, LogoComponent, ModelComponent, LoadingComponent }
