b2cc4d9b68bf501c77281f58e2911b71347bd26a
João Leão <joaofleao@gmail.com>
2023-01-12 04:27:23 -0300

    feat: screen index updated

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,4 +1,10 @@
+import HomeScreen from './Logged/HomeScreen'
+
 import SignInScreen from './Unlogged/SignInScreen'
+import SignUpEmailScreen from './Unlogged/SignUpEmailScreen'
+import SignUpPasswordScreen from './Unlogged/SignUpPasswordScreen'
+import LoadingScreen from './Unprotected/LoadingScreen'
+
 import SplashScreen from './Unprotected/SplashScreen'
 
-export { SignInScreen, SplashScreen }
+export { HomeScreen, SignInScreen, SplashScreen, SignUpEmailScreen, SignUpPasswordScreen, LoadingScreen }
