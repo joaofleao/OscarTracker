882abfbaf17de29a463c02979df59ac8fbdb1e0f
João Leão <joaofleao@gmail.com>
2023-01-12 04:18:18 -0300

    feat: loading component added

---
+++ src/components/LoadingComponent/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import LottieView from 'lottie-react-native'
+
+import { dots, movie } from '../../assets/animations'
+
+type LoadingComponentProps = {
+  animation?: string
+  speed?: number
+  size?: number
+}
+
+const LoadingComponent = ({ animation = 'dots', size = 30 }: LoadingComponentProps) => {
+  const render = animation === 'dots' ? dots : movie
+  const speed = animation === 'dots' ? 1 : 0.5
+  return (
+    <LottieView
+      style={{ width: size, height: size }}
+      autoPlay={true}
+      source={render}
+      speed={speed}
+      loop={true}
+    />
+  )
+}
+
+export default LoadingComponent
