f785ae82fb31c1f10091fb0d738c727f58b5413b
João Leão <joaofleao@gmail.com>
2023-01-25 00:18:54 -0300

    feature: model component

--- src/components/ModelComponent/index.tsx
+++ src/components/ModelComponent/index.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { HeaderComponent } from '../../components'
 
 interface ModelProps {
+  top?: boolean
+  bottom?: boolean
+  left?: boolean
+  right?: boolean
   children: React.ReactNode
 }
-function ModelComponent({ children, ...rest }: ModelProps) {
+function ModelComponent({ children, top = true, bottom = true, left = true, right = true, ...rest }: ModelProps) {
   const insets = useSafeAreaInsets()
+
+  const margins = {
+    paddingTop: top ? insets.top : 0,
+    paddingBottom: bottom ? insets.bottom : 0,
+    paddingLeft: left ? insets.left : 0,
+    paddingRight: right ? insets.right : 0,
+  }
+
   return (
     <View
       className='flex-1 bg-zinc-900 '
-      style={{
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-        paddingLeft: insets.left,
-        paddingRight: insets.right,
-      }}>
+      style={margins}>
       {children}
     </View>
   )
