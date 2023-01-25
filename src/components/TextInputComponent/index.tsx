7624451e733df6a58706c2c092449325a54d6003
João Leão <joaofleao@gmail.com>
2023-01-25 00:15:30 -0300

    feature: icon added to input

--- src/components/TextInputComponent/index.tsx
+++ src/components/TextInputComponent/index.tsx
@@ -2,6 +2,7 @@ import { NativeSyntheticEvent, TextInput, TextInputChangeEventData, View } from 
 import { styled } from 'nativewind'
 
 import colors from 'tailwindcss/colors'
+import { IconComponent } from '../../components'
 
 interface TextInputProps {
   appearance?: 'primary' | 'secondary' | 'dark' | 'light'
@@ -11,23 +12,32 @@ interface TextInputProps {
   type?: 'text' | 'password' | 'number'
   conditions?: 'email' | 'phone' | 'password'
   className?: string
-  disabled?: boolean
+  search?: boolean
   loading?: boolean
   onChange: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void
 }
 
-function TextInputComponent({ placeholder, onChange, value, ...rest }: TextInputProps) {
+function TextInputComponent({ placeholder, onChange, value, search, ...rest }: TextInputProps) {
   return (
     <View
-      className='border-solid border-stone-300 border rounded-xl justify-center items-center '
+      className='flex-row border-solid border-stone-300 border rounded-2xl justify-center items-center px-4'
       {...rest}>
+      {search && (
+        <IconComponent
+          name='search'
+          size={24}
+          color={colors.stone[700]}
+          className='mr-3'
+        />
+      )}
+
       <TextInput
         value={value}
         autoCapitalize='none'
         placeholder={placeholder}
-        placeholderTextColor={colors.gray[600]}
+        placeholderTextColor={colors.stone[700]}
         onChange={onChange}
-        className='text-white px-4 py-4 text-[16px] w-full font-primarySemibold '
+        className='mt-1 text-white py-4 text-[16px] flex-1 font-primarySemibold '
       />
     </View>
   )
