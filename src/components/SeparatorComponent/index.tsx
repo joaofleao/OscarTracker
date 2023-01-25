79af16356930ec1fcca72ecaa71b07658bd0b4d9
João Leão <joaofleao@gmail.com>
2023-01-25 00:14:23 -0300

    feature: separtor component

---
+++ src/components/SeparatorComponent/index.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+import { styled } from 'nativewind'
+
+function SeparatorComponent({ ...rest }) {
+  return (
+    <View
+      className={`w-5 h-5`}
+      {...rest}
+    />
+  )
+}
+
+export default styled(SeparatorComponent)
