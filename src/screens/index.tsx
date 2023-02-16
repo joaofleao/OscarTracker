ed01df9d2b1ac9cbad6f0cf1ff04517505d4c3f2
João Leão <joaofleao@gmail.com>
2023-02-16 14:56:29 -0300

    feat: updating the onboarding process

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -3,6 +3,8 @@ export { default as SplashScreen } from './SplashScreen'
 export { default as SignInScreen } from './Unlogged/SignInScreen'
 export { default as SignUpEmailScreen } from './Unlogged/SignUpEmailScreen'
 export { default as SignUpPasswordScreen } from './Unlogged/SignUpPasswordScreen'
+export { default as SignUpNameScreen } from './Unlogged/SignUpNameScreen'
+export { default as SignUpAvatarScreen } from './Unlogged/SignUpAvatarScreen'
 
 export { default as HomeScreen } from './Logged/HomeScreen'
 export { default as ProfileScreen } from './Logged/ProfileScreen'
