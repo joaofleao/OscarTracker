8204103cc2d7cea371aa9d870167fe691dc232ef
João Leão <joaofleao@gmail.com>
2023-02-17 04:02:43 -0300

    feat: progress bar update

--- src/components/ProgressBarComponent/index.tsx
+++ src/components/ProgressBarComponent/index.tsx
@@ -1,12 +1,9 @@
-import { TouchableOpacity, Text, View } from 'react-native'
-import { IconComponent } from '..'
+import { Text, View } from 'react-native'
 import { styled } from 'nativewind'
 
-import colors from 'tailwindcss/colors'
-
 interface ProgressBarProps {
-  total?: number
-  progress?: number
+  total: number
+  progress: number
 }
 
 function ProgressBarComponent({ total, progress, ...rest }: ProgressBarProps) {
@@ -18,7 +15,7 @@ function ProgressBarComponent({ total, progress, ...rest }: ProgressBarProps) {
       <View className='bg-zinc-800 h-2 flex-1 rounded-3xl'>
         <View
           className='bg-amber-500 flex-1 rounded-3xl'
-          style={{ width: (13 / 53) * 100 + '%' }}
+          style={{ width: (progress / total) * 100 + '%' }}
         />
       </View>
       <Text className='text-zinc-600 ml-4 font-primaryRegular '>{total}</Text>
