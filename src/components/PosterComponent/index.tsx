35384bdfc836804aae2802fec9b23934a4d2487a
João Leão <joaofleao@gmail.com>
2023-02-21 21:54:36 -0300

    feat: poster component added

---
+++ src/components/PosterComponent/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Image, View } from 'react-native'
+import { IconComponent } from '../../components'
+interface PosterProps {
+  image: string
+  isWatched: boolean
+  spoiler: boolean
+}
+
+function PosterComponent({ image, isWatched, spoiler }: PosterProps) {
+  const getPoster = (
+    <Image
+      source={{ uri: image }}
+      className='absolute w-full h-full rounded-xl'
+      resizeMode='cover'
+    />
+  )
+  const getCover = (
+    <View className='absolute w-[106px] h-[158px] rounded-xl bg-zinc-900/80 items-center justify-center' />
+  )
+  const getIcon = (
+    <View className='absolute rounded-xl bg-amber-500 p-2 justify-center items-center'>
+      <IconComponent
+        name='eye-off'
+        className='color-zinc-900'
+        size={18}
+      />
+    </View>
+  )
+
+  return (
+    <View className='w-[106px] h-[158px] items-center justify-center bg-zinc-700/20 rounded-xl relative'>
+      {(isWatched || spoiler) && getPoster}
+      {!isWatched && spoiler && getCover}
+      {!isWatched && getIcon}
+    </View>
+  )
+}
+
+export default PosterComponent
