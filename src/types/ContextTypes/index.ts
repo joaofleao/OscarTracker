86745cdf3eab617876c315cf5d069b592ec48916
João Leão <joaofleao@gmail.com>
2023-02-24 00:46:58 -0300

    fix: name of te variable on signup

--- src/types/ContextTypes/index.ts
+++ src/types/ContextTypes/index.ts
@@ -2,7 +2,7 @@ import { PreferencesType, User } from '../../types'
 
 export type AuthContextType = {
   signIn: (email: string, password: string) => Promise<boolean>
-  signUp: (email: string, password: string, displayName: string, avatar: any) => Promise<boolean>
+  signUp: (email: string, password: string, displayName: string, nickName: any) => Promise<boolean>
   signOut: () => void
   setUser: (user: any) => void
   initializing: boolean
