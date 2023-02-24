36d5462c5b4b04f5345f5ee3b0cb0c5e7e2bb8d5
João Leão <joaofleao@gmail.com>
2023-02-24 00:42:24 -0300

    fix: impors on images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,9 +3,9 @@ import Google from './Google'
 import Facebook from './Facebook'
 import Rotten from './Rotten'
 import IMDB from './IMDB'
-const poster = require('../Onboarding/en-us.jpg')
-const justin = require('../Onboarding/justin_long.jpeg')
-const jason = require('../Onboarding/jason_lee.jpeg')
-const david = require('../Onboarding/david_cross.jpeg')
+const poster = require('./Onboarding/en-us.jpg')
+const justin = require('./Onboarding/justin_long.jpeg')
+const jason = require('./Onboarding/jason_lee.jpeg')
+const david = require('./Onboarding/david_cross.jpeg')
 
 export { Logo, Google, Facebook, Rotten, IMDB, poster, justin, jason, david }
