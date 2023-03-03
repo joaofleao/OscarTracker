fdc8cf675941c69a5bde77a98a685bbc74ede498
João Leão <joaofleao@gmail.com>
2023-03-03 03:46:15 -0300

    feat: get cast trailer and providers on the tmdb

--- src/providers/MoviesProvider.tsx
+++ src/providers/MoviesProvider.tsx
@@ -7,8 +7,17 @@ const MoviesProvider: React.FC<Provider> = ({ children }) => {
   const getMovie = async (id: string) => {
     return await tmdb.getMovie(id)
   }
+  const getCast = async (id: string) => {
+    return await tmdb.getCast(id)
+  }
+  const getTrailer = async (id: string) => {
+    return await tmdb.getVideos(id)
+  }
+  const getProviders = async (id: string) => {
+    return await tmdb.getProviders(id)
+  }
 
-  const value = { getMovie } satisfies MoviesContextType
+  const value = { getMovie, getCast, getTrailer, getProviders } satisfies MoviesContextType
 
   return <MoviesContext.Provider value={value}>{children}</MoviesContext.Provider>
 }
