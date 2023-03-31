068830e2f381eb6053521e9826e5f63366875a43
João Leão <joaofleao@gmail.com>
2023-03-31 20:03:21 -0300

    refactor: context types deleted

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,3 @@
 export type { BasicMovieType, Category, Nomination, PreferencesType, Provider, UserType } from './BasicTypes'
-export type { AuthContextType, DataContextType, MoviesContextType, ThemeContextType, ToastContextType, UserContextType } from './ContextTypes'
 export type { ScreenTypes } from './ScreenTypes'
 export type { User } from 'firebase/auth'
