c8311ecb36eccb80ba32932d6ce4a825badac8ec
João Leão <joaofleao@gmail.com>
2023-03-31 19:27:52 -0300

    refactor: eslint on AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,14 @@
 import { createContext } from 'react'
 
-import { type AuthContextType } from '../types'
+export interface AuthContextType {
+  signIn: (email: string, password: string) => Promise<void>
+  signUp: (email: string, password: string, displayName: string, nickName: any) => Promise<void>
+  signOut: () => Promise<void>
+  setUser: (user: any) => void
+  initializing: boolean
+}
 
-const AuthContext = createContext<AuthContextType>({} as AuthContextType)
+const AuthContext = createContext<AuthContextType | null>(null)
 AuthContext.displayName = 'AuthContext'
 
 export default AuthContext
