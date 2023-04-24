0e86db00f8fe0871d1c86bd5e8a989525e77a843
João Leão <joaofleao@gmail.com>
2023-04-24 00:25:15 -0300

    refactor: new export for screen types

--- src/types/ScreenTypes/index.ts
+++ src/types/ScreenTypes/index.ts
@@ -1,7 +1,7 @@
 import { type ParamListBase } from '@react-navigation/native'
 import { type NativeStackScreenProps } from '@react-navigation/native-stack'
 
-interface ScreenTypes extends ParamListBase {
+export interface ScreenTypes extends ParamListBase {
   IndexScreen: undefined
   SignInScreen: undefined
   SignUpEmailScreen: undefined
@@ -27,15 +27,13 @@ interface ScreenTypes extends ParamListBase {
   }
 }
 
-type HomeScreenProps = NativeStackScreenProps<ScreenTypes, 'HomeScreen'>
-type MovieScreenProps = NativeStackScreenProps<ScreenTypes, 'MovieScreen'>
-type PreferencesScreenProps = NativeStackScreenProps<ScreenTypes, 'PreferencesScreen'>
-type NominationScreenProps = NativeStackScreenProps<ScreenTypes, 'NominationScreen'>
-type SignInScreenProps = NativeStackScreenProps<ScreenTypes, 'SignInScreen'>
-type WatchListScreenProps = NativeStackScreenProps<ScreenTypes, 'WatchListScreen'>
-type SignUpAvatarScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpAvatarScreen'>
-type SignUpEmailScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpEmailScreen'>
-type SignUpPasswordScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpPasswordScreen'>
-type SignUpNameScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpNameScreen'>
-
-export type { HomeScreenProps, MovieScreenProps, NominationScreenProps, PreferencesScreenProps, ScreenTypes, SignInScreenProps, SignUpAvatarScreenProps, SignUpEmailScreenProps, SignUpNameScreenProps, SignUpPasswordScreenProps, WatchListScreenProps }
+export type HomeScreenProps = NativeStackScreenProps<ScreenTypes, 'HomeScreen'>
+export type MovieScreenProps = NativeStackScreenProps<ScreenTypes, 'MovieScreen'>
+export type PreferencesScreenProps = NativeStackScreenProps<ScreenTypes, 'PreferencesScreen'>
+export type NominationScreenProps = NativeStackScreenProps<ScreenTypes, 'NominationScreen'>
+export type SignInScreenProps = NativeStackScreenProps<ScreenTypes, 'SignInScreen'>
+export type WatchListScreenProps = NativeStackScreenProps<ScreenTypes, 'WatchListScreen'>
+export type SignUpAvatarScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpAvatarScreen'>
+export type SignUpEmailScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpEmailScreen'>
+export type SignUpPasswordScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpPasswordScreen'>
+export type SignUpNameScreenProps = NativeStackScreenProps<ScreenTypes, 'SignUpNameScreen'>
