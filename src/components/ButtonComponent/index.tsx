090e2be0d70d13a8bdc7d301d1841af9d92afd64
João Leão <joaofleao@gmail.com>
2023-05-04 01:30:48 -0300

    feat: refactor of the button and loading components

--- src/components/ButtonComponent/index.tsx
+++ src/components/ButtonComponent/index.tsx
@@ -1,73 +1,105 @@
-import React, { Text, TouchableOpacity, type TouchableOpacityProps, View } from 'react-native'
-import { styled } from 'nativewind'
-import colors from 'tailwindcss/colors'
+import React, { useRef } from 'react'
+import { Animated, type PressableProps } from 'react-native'
 
-import { IconComponent } from '../../components'
+import { LoadingComponent } from '../../components'
+import * as Styled from './styles'
 
-interface ButtonProps extends TouchableOpacityProps {
-  appearance?: 'primary' | 'secondary' | 'dark' | 'light'
-  name?: string
-  size?: 'small' | 'medium' | 'big' | 'full'
-  variant?: 'plain' | 'filled' | 'outlined'
-  disabled?: boolean
-  loading?: boolean
-  onPress?: () => void
+interface ButtonProps extends PressableProps {
+  label?: string
+  width: 'fit' | 'fixed' | 'full'
+  variant: 'primary' | 'secondary' | 'outlined' | 'text'
+  disabled: boolean
+  loading: boolean
   icon?: string
-  iconPositon?: 'trailing' | 'leading'
+  iconPositon: 'trailing' | 'leading'
 }
 
-const ButtonComponent = ({ name, variant = 'filled', iconPositon, icon, ...props }: ButtonProps): JSX.Element => {
-  const plain = variant === 'plain'
-  const outlined = variant === 'outlined'
-  const filled = variant === 'filled'
+const defaultValue = {
+  width: 'fixed',
+  variant: 'primary',
+  disabled: false,
+  loading: false,
+  iconPositon: 'leading',
+}
 
-  const getColor = (): string => {
-    if (plain) return colors.amber[500]
-    if (outlined) return colors.amber[500]
-    if (filled) return colors.zinc[900]
-    else return colors.amber[500]
-  }
+const ButtonComponent = (props: ButtonProps): JSX.Element => {
+  const { label, width, variant, disabled, loading, icon, iconPositon, ...rest } = props
+
+  const scaleAnimation = useRef(new Animated.Value(0)).current;
+  const opacityAnimation = useRef(new Animated.Value(0)).current;
+  const scale = scaleAnimation.interpolate({ inputRange: [0, 1], outputRange: [1, 0.95] });
+  const opacity = opacityAnimation.interpolate({ inputRange: [0, 1], outputRange: [1, 0.8] });
+
+  const onPressIn = (): void => {
+    Animated.spring(scaleAnimation, {
+      toValue: 1,
+      speed: 200,
+      useNativeDriver: true,
+    }).start();
+    Animated.spring(opacityAnimation, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+  const onPressOut = (): void => {
+    Animated.spring(scaleAnimation, {
+      toValue: 0,
+      speed: 200,
+      useNativeDriver: true,
+    }).start();
+    Animated.spring(opacityAnimation, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
-    <TouchableOpacity
-      className={` justify-center py-3
-        ${icon != null && iconPositon === undefined ? 'px-0' : 'px-4'}
-        ${name != null ? 'rounded-3xl' : 'rounded-2xl '}
-        ${plain ? '' : ''}
-        ${outlined ? 'border-solid border-2 border-amber-500' : ''}
-        ${filled ? 'bg-amber-500' : ''}
-        `}
-      {...props}
-    >
-      <View className="flex-row justify-center items-center ">
-        {icon != null && iconPositon === 'leading' && (
-          <IconComponent
-            name={icon}
-            size={24}
-            color={getColor()}
-            className={name != null ? 'mr-3' : ''}
-          />
-        )}
-
-        <Text
-          className={`text-lg font-primaryBold text-center flex-1`}
-          style={{ color: getColor() }}
-          numberOfLines={1}
-        >
-          {name}
-        </Text>
-
-        {icon != null && iconPositon === 'trailing' && (
-          <IconComponent
-            name={icon}
-            size={24}
-            color={getColor()}
-            className={name != null ? 'ml-3' : ''}
-          />
-        )}
-      </View>
-    </TouchableOpacity>
+    <Animated.View style={{ transform: [{ scale }], opacity }}>
+      <Styled.Button
+        width={width}
+        variant={variant}
+        disabled={disabled}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        {...rest}
+      >
+        {loading ?
+          <LoadingComponent disabled={disabled} animation={'circle'} size={24} type={variant === 'primary' ? 'secondary' : 'primary'} />
+          :
+          <>
+            {icon != null && iconPositon === 'leading' && (
+              <Styled.Icon
+                name={icon}
+                size={18}
+                variant={variant}
+                disabled={disabled}
+              />
+            )}
+            {label != null &&
+              <Styled.Label
+                numberOfLines={1}
+                variant={variant}
+                disabled={disabled}
+              >
+                {label}
+              </Styled.Label>
+            }
+            {icon != null && iconPositon === 'trailing' && (
+              <Styled.Icon
+                name={icon}
+                width={24}
+                height={24}
+                variant={variant}
+                disabled={disabled}
+              />
+            )}
+          </>}
+      </Styled.Button>
+    </Animated.View>
   )
 }
 
-export default styled(ButtonComponent)
+ButtonComponent.defaultProps = defaultValue
+
+export default ButtonComponent
+
