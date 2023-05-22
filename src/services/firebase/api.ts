00010d995da577ad38950692ab57cabba99e37f2
João Leão <joaofleao@gmail.com>
2023-05-21 23:45:05 -0300

    feat: firebase for android working

--- src/services/firebase/api.ts
+++ src/services/firebase/api.ts
@@ -1,15 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth/react-native'
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore } from 'firebase/firestore'
 
 import { firebaseConfig } from './config'
 
 const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
 
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
+})
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
 })
 
 export { auth, db }
