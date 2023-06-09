3698fd74ae3c30ae188038d746ab55240e465a3a
João Leão <joaofleao@gmail.com>
2023-06-09 20:02:00 -0300

    feature: types created for tmdb data

--- src/services/tmdb/api.ts
+++ src/services/tmdb/api.ts
@@ -1,5 +1,6 @@
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
+import type { CastType, MovieType, ProvidersType } from '../../types'
 import { tmdbConfig } from './config'
 
 const apiBaseUrl = tmdbConfig.api_base_url
@@ -11,36 +12,34 @@ const api = axios.create({
   timeout: 60000,
 })
 
-const getImage = (id: string): any => {
+export const getImage = (id: string): any => {
   return imageBaseUrl + id
 }
 
-const getMovie = async (id: string): Promise<any> => {
-  const response = await api.get(`movie/${id}`, {
-    params: { api_key: apiKey, language: 'en-US' },
+export const getMovie = async (id: string, language: string): Promise<AxiosResponse<MovieType>> => {
+  const response: AxiosResponse<MovieType> = await api.get(`movie/${id}`, {
+    params: { api_key: apiKey, language },
   })
-  return response.data
+  return response
 }
 
-const getCast = async (id: string): Promise<any> => {
-  const response = await api.get(`movie/${id}/credits`, {
-    params: { api_key: apiKey, language: 'en-US' },
+export const getCast = async (id: string, language: string): Promise<AxiosResponse<CastType>> => {
+  const response: AxiosResponse<CastType> = await api.get(`movie/${id}/credits`, {
+    params: { api_key: apiKey, language },
   })
-  return response.data
+  return response
 }
 
-const getVideos = async (id: string): Promise<any> => {
-  const response = await api.get(`movie/${id}/videos`, {
-    params: { api_key: apiKey, language: 'en-US' },
+export const getVideos = async (id: string, language: string): Promise<AxiosResponse<any>> => {
+  const response: AxiosResponse<any> = await api.get(`movie/${id}/videos`, {
+    params: { api_key: apiKey, language },
   })
-  return response.data
+  return response
 }
 
-const getProviders = async (id: string): Promise<any> => {
-  const response = await api.get(`movie/${id}/watch/providers`, {
-    params: { api_key: apiKey, language: 'en-US' },
+export const getProviders = async (id: string, language: string): Promise<AxiosResponse<ProvidersType>> => {
+  const response: AxiosResponse<ProvidersType> = await api.get(`movie/${id}/watch/providers`, {
+    params: { api_key: apiKey, language },
   })
-  return response.data
+  return response
 }
-
-export { getCast, getImage, getMovie, getProviders, getVideos }
