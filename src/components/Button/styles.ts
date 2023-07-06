be2ad8cee6a17da16584b879f2dfb4721790b7b8
João Leão <joaofleao@gmail.com>
2023-07-05 23:09:16 -0300

    feat: button new name

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -31,22 +31,22 @@ const getBorder = (props: Props): string => {
 }
 const getWidth = (props: Props): string => {
   if (props.width === 'fit') return 'auto'
-  if (props.width === 'fixed') return props.theme.spacing.space256
+  if (props.width === 'fixed') return props.theme.spacings.space31
   return '100%'
 }
 const getHorizontalPadding = (props: Props): any => {
-  if (props.label !== null) return { paddingHorizontal: props.theme.spacing.space16 }
-  if (props.icon !== null && props.iconPositon === 'leading') return { paddingRight: props.theme.spacing.space24, paddingLeft: props.theme.spacing.space20 }
-  if (props.icon !== null && props.iconPositon === 'trailing') return { paddingRight: props.theme.spacing.space20, paddingLeft: props.theme.spacing.space24 }
-  return { paddingHorizontal: props.theme.spacing.space24 }
+  if (props.label !== null) return { paddingHorizontal: props.theme.spacings.space16 }
+  if (props.icon !== null && props.iconPositon === 'leading') return { paddingRight: props.theme.spacings.space24, paddingLeft: props.theme.spacings.space20 }
+  if (props.icon !== null && props.iconPositon === 'trailing') return { paddingRight: props.theme.spacings.space20, paddingLeft: props.theme.spacings.space24 }
+  return { paddingHorizontal: props.theme.spacings.space24 }
 }
 
 export const Pressable = styled.Pressable((props: Props) => ({
   background: getBackgroundColor(props),
   border: getBorder(props),
-  paddingVertical: props.theme.spacing.space16,
+  paddingVertical: props.theme.spacings.space9,
   ...getHorizontalPadding(props),
-  borderRadius: props.theme.spacing.space20,
+  borderRadius: props.theme.spacings.space10,
   width: getWidth(props),
   justifyContent: 'center',
   alignItems: 'center',
@@ -55,8 +55,8 @@ export const Pressable = styled.Pressable((props: Props) => ({
 
 export const Label = styled.Text((props: Props) => ({
   fontFamily: props.theme.typography.primary.bold,
-  fontSize: props.theme.typography.size.font16,
-  lineHeight: props.theme.typography.size.font20,
+  fontSize: props.theme.typography.sizes.fontSize3,
+  lineHeight: props.theme.typography.sizes.fontSize5,
   color: getContentColor(props),
   maxWidth: '80%',
   textAlign: 'center',
@@ -64,8 +64,8 @@ export const Label = styled.Text((props: Props) => ({
 
 export const Icon = styled(IconComponent)((props: Props) => ({
   color: getContentColor(props),
-  fontSize: props.theme.typography.size.font20,
-  lineHeight: props.theme.typography.size.font20,
-  marginRight: props.name !== null && props.iconPositon === 'leading' && props.label === null && props.theme.spacing.space8,
-  marginLeft: props.name !== null && props.iconPositon === 'trailing' && props.label === null && props.theme.spacing.space8,
+  fontSize: props.theme.typography.sizes.fontSize5,
+  lineHeight: props.theme.typography.sizes.fontSize5,
+  marginRight: props.name !== null && props.iconPositon === 'leading' && props.label === null && props.theme.spacings.space8,
+  marginLeft: props.name !== null && props.iconPositon === 'trailing' && props.label === null && props.theme.spacings.space8,
 }))
