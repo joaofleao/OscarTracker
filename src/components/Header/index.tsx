062df6a415c3054d5f40c933a4e8a84d6e5b5681
João Leão <joaofleao@gmail.com>
2023-08-21 20:09:57 -0300

    feat: header removed wrapper

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,4 @@
-import React from 'react'
-
-import Wrapper, { type WrapperProps } from '../Wrapper'
-import HeaderComponent, { type HeaderProps } from './Header'
-
-const Header = (props: HeaderProps & WrapperProps): JSX.Element => {
-  const { mt, mv, mh, mb, mr, ml, ...rest } = props
-  return (
-    <Wrapper
-      mt={mt}
-      mv={mv}
-      mh={mh}
-      mb={mb}
-      mr={mr}
-      ml={ml}
-    >
-      <HeaderComponent {...rest} />
-    </Wrapper>
-  )
-}
+import Header, { type HeaderProps } from './Header'
 
 export default Header
-export type { HeaderComponent }
+export type { HeaderProps }
