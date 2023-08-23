a0b39cfbae827533fe6b4c7a9f9db057d867f087
João Leão <joaofleao@gmail.com>
2023-08-23 13:19:34 -0300

    fix: remaning icons

--- src/components/Global/styles.ts
+++ src/components/Global/styles.ts
@@ -1,9 +1,8 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
 
 import type { StyledProps } from '../../types'
 
-export const Screen = styled(SafeAreaView)((props: StyledProps) => ({
+export const Screen = styled.SafeAreaView((props: StyledProps) => ({
   backgroundColor: props.theme.palette.background.default,
   flex: 1,
 }))
