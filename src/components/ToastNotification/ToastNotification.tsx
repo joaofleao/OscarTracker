107a85743ea3e09a9640173e6c8c758ec9eba807
João Leão <joaofleao@gmail.com>
2023-08-23 14:34:45 -0300

    feat: notification changed to styled

---
+++ src/components/ToastNotification/ToastNotification.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import type { Animated, ViewProps } from 'react-native'
+
+import { IconComponent } from '../../components'
+import { useTheme } from '../../features'
+import * as Styled from './styles'
+
+export interface ToastNotificationProps extends ViewProps {
+  title: string
+  description: string
+  type: string
+  position: Animated.Value
+}
+
+const ToastNotification = (props: ToastNotificationProps): JSX.Element => {
+  const { title, description, type, position, ...rest } = props
+  const theme = useTheme()
+  const isSuccess = type === 'success'
+
+  return (
+    <Styled.Container
+      style={{ transform: [{ translateY: position }] }}
+      isSuccess={isSuccess}
+      {...rest}
+    >
+      <Styled.Content>
+        <IconComponent
+          name={isSuccess ? 'check-circle' : 'alert-circle'}
+          color={theme.palette.text.default}
+          size={30}
+        />
+
+        <Styled.Informations>
+          <Styled.Title numberOfLines={2}>{title}</Styled.Title>
+          {description != null && <Styled.Description>{description}</Styled.Description>}
+        </Styled.Informations>
+      </Styled.Content>
+
+      <IconComponent
+        name="x"
+        color={theme.palette.text.default}
+        size={24}
+      />
+    </Styled.Container>
+  )
+}
+
+export default ToastNotification
