0377d3a97117de0a109e022af606189fa535f4a4
João Leão <joaofleao@gmail.com>
2023-09-08 11:21:10 -0300

    feat: configs changed to allow @

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,23 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@features': './src/features',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@styles': './src/styles',
+            '@types': './src/types',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   }
 }
