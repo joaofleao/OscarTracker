43494cdab8b7adc7f81d5bec443747bcc6bb6674
João Leão <joaofleao@gmail.com>
2023-09-20 03:39:15 -0300

    fix: toast notification added back

--- src/features/toast/ToastProvider.tsx
+++ src/features/toast/ToastProvider.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { Animated, Dimensions } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import ToastContext, { type ToastContextType } from './ToastContext'
-// import ToastNotification  from '@components/ToastNotification'
+import ToastNotification from '@components/ToastNotification'
 import { useLoading } from '@features/loading'
 
 const ToastProvider = ({ children }: { children?: React.ReactNode }): JSX.Element => {
   const loading = useLoading()
-  // const [title, setTitle] = useState<string>('')
-  // const [description, setDescription] = useState<string>('')
-  // const [type, setType] = useState<string>('')
-  const [toast, setToast] = useState<boolean>(false)
+  const [title, setTitle] = React.useState<string>('')
+  const [description, setDescription] = React.useState<string>('')
+  const [type, setType] = React.useState<string>('')
+  const [toast, setToast] = React.useState<boolean>(false)
   const insets = useSafeAreaInsets()
 
   const windowHeight = Dimensions.get('window').height
-  const popAnim = useRef(new Animated.Value(windowHeight * -1)).current
+  const popAnim = React.useRef(new Animated.Value(windowHeight * -1)).current
 
-  useEffect(() => {
+  React.useEffect(() => {
     const popIn = (): void => {
       Animated.timing(popAnim, {
-        toValue: insets.top,
+        toValue: insets.top + 20,
         duration: 300,
         useNativeDriver: true,
       }).start()
@@ -44,12 +44,11 @@ const ToastProvider = ({ children }: { children?: React.ReactNode }): JSX.Elemen
     }
   }, [loading.isActive, toast, insets.top, popAnim, windowHeight])
 
-  const showToast = () // title: string, description: string, type: 'success' | 'error'
-  : void => {
-    // setTitle(title)
-    // setDescription(description)
-    // setType(type)
-    // setToast(true)
+  const showToast = (_title: string, _description: string, _type: 'success' | 'error'): void => {
+    setTitle(_title)
+    setDescription(_description)
+    setType(_type)
+    setToast(true)
   }
 
   const value = {
@@ -58,12 +57,12 @@ const ToastProvider = ({ children }: { children?: React.ReactNode }): JSX.Elemen
 
   return (
     <ToastContext.Provider value={value}>
-      {/* <ToastNotification
+      <ToastNotification
         title={title}
         description={description}
         type={type}
         position={popAnim}
-      /> */}
+      />
       {children}
     </ToastContext.Provider>
   )
