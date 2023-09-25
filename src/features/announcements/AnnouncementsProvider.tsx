c5473820423bcf39dddde0c6a5e911d9a37166e6
João Leão <joaofleao@gmail.com>
2023-09-25 01:33:13 -0300

    fix: announcements fetched first render

--- src/features/announcements/AnnouncementsProvider.tsx
+++ src/features/announcements/AnnouncementsProvider.tsx
@@ -2,19 +2,16 @@ import React from 'react'
 import { collection, getDocs, orderBy, query } from 'firebase/firestore'
 
 import AnnouncementsContext, { type AnnouncementsContextType } from './AnnouncementsContext'
-import { useApp } from '@features/app'
 import { db } from '@services/firebase'
 import type { AnnouncementType } from '@types'
 import { printFetch } from '@utils/functions'
 
 const AnnouncementProvider = ({ children }: { children?: React.ReactNode }): JSX.Element => {
   const announcementsCollection = collection(db, 'announcements')
-  const app = useApp()
 
   const [announcements, setAnnouncements] = React.useState<AnnouncementType[]>([])
 
   const getAnnouncements = async (): Promise<void> => {
-    if (!app.hasInternet) return
     const orderedAnnouncements = query(announcementsCollection, orderBy('date'))
     const response = await getDocs(orderedAnnouncements)
     printFetch('Firebase', 'Announcements fetched', 'yellow')
---
+++ src/screens/Unlogged/SignUp/EmailVerification/index.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+import * as Styled from './styles'
+import Button from '@components/Button'
+import Global from '@components/Global'
+import Header from '@components/Header'
+import Icon from '@components/Icon'
+import Input from '@components/Input'
+import { type EmailProps } from '@types'
+import routes from '@utils/routes'
+
+const Email = ({ navigation }: EmailProps): JSX.Element => {
+  const [email, setEmail] = useState<string>('')
+
+  const formattedEmail = email.replace(/[^a-zA-Z0-9@.]/g, '')
+
+  const emailValid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+  const handleNext = (): void => {
+    navigation.navigate(routes.unlogged.signUpPassword, {
+      email: formattedEmail,
+    })
+  }
+
+  return (
+    <Global.Screen>
+      <Header.Root>
+        <Button
+          onPress={navigation.goBack}
+          icon={<Icon.ArrowLeft />}
+          variant="secondary"
+        />
+        <Header.TextContainer>
+          <Header.Title>Register</Header.Title>
+        </Header.TextContainer>
+      </Header.Root>
+
+      <Styled.Header>
+        <Global.Title>Give us your best e-mail</Global.Title>
+        <Global.Description>We will send you a verification code to confirm</Global.Description>
+      </Styled.Header>
+
+      <Styled.Content>
+        <Input
+          autoComplete="email"
+          label="Email"
+          value={email}
+          validation={emailValid}
+          errorText={'You must provide a valid email'}
+          onChangeText={setEmail}
+        />
+      </Styled.Content>
+
+      <Styled.Footer>
+        <Button
+          width="fixed"
+          label="Next"
+          disabled={!emailValid}
+          onPress={handleNext}
+        />
+      </Styled.Footer>
+    </Global.Screen>
+  )
+}
+
+export default Email
